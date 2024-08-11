b8a3f828efb1dbcc0cd9e0aa38c1aece6463bf37
simaaksidd <simaaksidd1@gmail.com>
2024-08-11 15:58:01 -0500

    add base dir path for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV = 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/portfolio_2024' : '',
     output: 'export',
     distDir: 'dist',
     images:{
