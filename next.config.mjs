accb140c0daa7bb669f4de1184eb67d6bfb29917
simaaksidd <simaaksidd1@gmail.com>
2024-08-11 16:01:03 -0500

    fix basepath issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV = 'production';
+ // const isProd = process.env.NODE_ENV = 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/portfolio_2024' : '',
+    // basePath: isProd ? '/portfolio_2024' : '',
     output: 'export',
     distDir: 'dist',
     images:{
