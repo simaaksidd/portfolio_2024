75e0255fba84017ef062aa61ce4826e2b8807029
simaaksidd <simaaksidd1@gmail.com>
2024-08-15 13:25:19 -0500

    fix pipeline

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React from "react";
-import motion from "framer-motion";
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 const PortfolioPage = () => {
