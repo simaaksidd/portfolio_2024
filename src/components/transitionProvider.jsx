1eb32fa5632964fd2ad31b8b332ae435942a6e4a
simaaksidd <simaaksidd1@gmail.com>
2025-03-12 18:16:51 -0500

    fix transition provider bug which blocks content

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -1,13 +1,14 @@
 "use client"
 
 import Navbar from "./navbar"
-import { AnimatePresence } from "framer-motion"
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 import { usePathname } from "next/navigation"
+import { useState, useEffect } from "react"
 
 const TransitionProvider = ({ children }) => {
-
     const OriginalPathName = usePathname()
+    const [showText, setShowText] = useState(true)
+    
     let pathName;
     if (OriginalPathName === '/') {
         pathName = 'Welcome';
@@ -15,8 +16,13 @@ const TransitionProvider = ({ children }) => {
         pathName = OriginalPathName.substring(1)
         pathName = pathName.charAt(0).toUpperCase() + pathName.substring(1)
     }
+
+    useEffect(() => {
+        setShowText(true)
+    }, [OriginalPathName])
+
     return (
-        <AnimatePresence mode="wait">
+        <AnimatePresence mode="wait" >
             <div key={pathName} className='w-full h-full bg-gradient-to-b from-gray-100 to-white'>
                 <motion.div 
                     className="h-screen w-screen fixed bg-black rounded-b-[100px] z-30"
@@ -24,15 +30,18 @@ const TransitionProvider = ({ children }) => {
                     exit={{height: "140vh" }}
                     transition={{duration:0.5, ease:"easeOut"}}
                 />
-                <motion.div 
-                    className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-30 w-fit h-fit"
-                    initial={{opacity: 1}}
-                    animate={{opacity: 0 }}
-                    exit={{opacity: 0 }}
-                    transition={{duration:0.8, ease:"easeOut"}}
-                >
-                    {pathName}
-                </motion.div>
+                {showText && (
+                    <motion.div 
+                        className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-30 w-fit h-fit"
+                        initial={{opacity: 1}}
+                        animate={{opacity: 0 }}
+                        exit={{ opacity: 0 }}
+                        transition={{duration:0.8, ease:"easeOut"}}
+                        onAnimationComplete={() => setShowText(false)}
+                    >
+                        {pathName}
+                    </motion.div>
+                )}
                 <motion.div 
                     className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-20"
                     initial={{height:"140vh"}}
