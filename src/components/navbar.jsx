08bc5d2c6444a85438c391d1248a0d5fea0afd80
simaaksidd <simaaksidd1@gmail.com>
2025-07-26 16:20:05 -0500

    remove about seciton, newgradify

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,6 @@ import { motion } from "framer-motion";
 
 const links = [
     {url: "./", title: "Home"},
-    {url: "./about", title: "About"},
     {url: "./portfolio", title: "Portfolio"},
     {url: "./contact", title: "Contact"},
 ];
