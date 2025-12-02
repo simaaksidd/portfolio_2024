b095c6f541859c330082e59fe7e00c03e0dd0e5a
simaaksidd <simaaksidd1@gmail.com>
2025-12-02 12:46:08 -0600

    Configure Next.js to allow GitHub user content

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,26 @@
  // const isProd = process.env.NODE_ENV = 'production';
 
 const nextConfig = {
-    // basePath: isProd ? '/portfolio_2024' : '',
-    output: 'export',
-    distDir: 'dist',
-    images:{
-        unoptimized: true,
-        remotePatterns:[
-            {
-                protocol: "https",
-                hostname: "images.pexels.com",
-            },
-        ],
-    },
+  // basePath: isProd ? '/portfolio_2024' : '',
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
